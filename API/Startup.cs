813dd102bbdfaef3a5e43be25a728f2c4c8e3a38
Calum Macleod <calummacleod88@gmail.com>
2021-02-07 08:52:02 +1300

    angular skeleton in place

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -53,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            // This has to go here - after routing and before useAuth
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
